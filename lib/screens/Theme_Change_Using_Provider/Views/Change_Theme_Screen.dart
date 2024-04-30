5b044d3fee9ddc77f19b95e124de2c5e7b029dfc
Admin <maayush235@gmail.com>
2024-04-30 08:26:42 +0530

    Radhey Radhey

--- lib/screens/Theme_Change_Using_Provider/Views/Change_Theme_Screen.dart
+++ lib/screens/Theme_Change_Using_Provider/Views/Change_Theme_Screen.dart
@@ -8,121 +8,106 @@ class ThemeChange extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      themeMode: Provider.of<ThemeProvider>(context, listen: true).isdark ? ThemeMode.light:ThemeMode.dark,
-      theme: ThemeData(
-      colorScheme: ColorScheme.light(
-        primary: Colors.blue,
-        secondary: Colors.red,
-
-      )  ,
-      ),
-      darkTheme: ThemeData(
-        primaryColor: Colors.teal,
-        secondaryHeaderColor: Colors.green
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          leading: Icon(Icons.arrow_back_ios),
-          title: Text(
-            'Theme App',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-          ),
-          actions: [Icon(Icons.add_circle_outline)],
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios),
+        title: Text(
+          'Theme App',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
         ),
-        body: Center(
-            child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              CircleAvatar(
-                radius: 75,
-              ),
-              Text(
-                'Testing User',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: [
-                  Icon(Icons.sunny),
-                  SizedBox(
-                    width: 18,
-                  ),
-                  Text(
-                    'Light Mode',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                  SizedBox(
-                    width: 140,
-                  ),
-                  Switch(
-                    value: Provider.of<ThemeProvider>(context, listen: true).isdark,
-                    onChanged: (value) {
-                      Provider.of<ThemeProvider>(context, listen: false)
-                          .themechange(value);
-                    },
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(Icons.grid_on_rounded),
-                  SizedBox(
-                    width: 20,
-                  ),
-                  Text(
-                    'Story',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(Icons.settings),
-                  SizedBox(
-                    width: 18,
-                  ),
-                  Text(
-                    'Settings and Privacy',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(Icons.speaker_notes_outlined),
-                  SizedBox(
-                    width: 18,
-                  ),
-                  Text(
-                    'Help center',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  Icon(Icons.notifications),
-                  SizedBox(
-                    width: 18,
-                  ),
-                  Text(
-                    'Notifications',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 20,)
-            ],
-          ),
-        )),
+        actions: [Icon(Icons.add_circle_outline)],
       ),
+      body: Center(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            CircleAvatar(
+              radius: 75,
+            ),
+            Text(
+              'Testing User',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                Icon(Icons.sunny),
+                SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  'Light Mode',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+                SizedBox(
+                  width: 140,
+                ),
+                Switch(
+                  value: Provider.of<ThemeProvider>(context, listen: true).isdark,
+                  onChanged: (value) {
+                    Provider.of<ThemeProvider>(context, listen: false)
+                        .themechange(value);
+                  },
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.grid_on_rounded),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'Story',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.settings),
+                SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  'Settings and Privacy',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.speaker_notes_outlined),
+                SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  'Help center',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Icon(Icons.notifications),
+                SizedBox(
+                  width: 18,
+                ),
+                Text(
+                  'Notifications',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20,)
+          ],
+        ),
+      )),
     );
   }
 }
