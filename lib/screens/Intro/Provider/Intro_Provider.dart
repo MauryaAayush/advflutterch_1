d7483484838a7388815158ac98c60a2390647349
Admin <maayush235@gmail.com>
2024-04-30 19:11:35 +0530

    Radhey Radhey

--- lib/screens/Intro/Provider/Intro_Provider.dart
+++ lib/screens/Intro/Provider/Intro_Provider.dart
@@ -5,9 +5,23 @@ class IntroProvider extends ChangeNotifier
 {
   bool ischecked = false;
 
+
+  // set
   Future<void> setdata()
   async {
     SharedPreferences preferences = await SharedPreferences.getInstance();
     preferences.setBool( ischecked as String, true);
   }
+
+
+  // get
+Future<void> GetResponse()
+async {
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  bool isclicked = preferences.getBool(ischecked as String) ?? false;
+}
+
+
+
+
 }
