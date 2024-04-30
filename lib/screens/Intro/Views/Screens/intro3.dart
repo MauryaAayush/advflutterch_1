b258102c71f70e0752a07cf727dc902fdce61975
Admin <maayush235@gmail.com>
2024-04-30 18:38:17 +0530

    Radhey Radhey

---
+++ lib/screens/Intro/Views/Screens/intro3.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Intro3 extends StatelessWidget {
+  const Intro3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Intro3'),
+      ),
+    );
+  }
+}
