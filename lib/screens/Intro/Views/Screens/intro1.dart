a533ef6433df5285232e30b78371c7d45b66905f
Admin <maayush235@gmail.com>
2024-05-01 14:22:52 +0530

    Radhey Radhey

--- lib/screens/Intro/Views/Screens/intro1.dart
+++ lib/screens/Intro/Views/Screens/intro1.dart
@@ -1,5 +1,7 @@
 import 'package:advflutterch_1/screens/Intro/Views/Screens/intro2.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Intro1 extends StatelessWidget {
   const Intro1({super.key});
@@ -7,17 +9,120 @@ class Intro1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Intro1"),
-        ),
-        body: Center(child: ElevatedButton(onPressed: () {
-          Navigator.of(context).push(
-              MaterialPageRoute(builder: (context) => const Intro2()),);
-        }, child: Text('Next',style: TextStyle(
-          fontSize: 25,
-          color: Colors.white
-        ),)),)
+        // appBar: AppBar(
+        //   backgroundColor: Colors.white,
+        //   title: Text("Intro1"),
+        // ),
+        body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 400,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                color: Colors.white,
+                image: DecorationImage(image: AssetImage('assets/images/1.png'))),
+                      ),
 
-    );
+                      SizedBox(
+                        height: 10,
+                      ),
+
+                      Text(
+                        'Quick Organize',
+                        style: TextStyle(
+              fontSize: 26,
+              fontWeight: FontWeight.w500,
+              letterSpacing: 2,
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Text(
+                        "            The file App simplifies document management with intuitive tools , secure cloud integration.",
+                        style: TextStyle(
+              color: Colors.grey,
+              fontSize: 18,
+              fontWeight: FontWeight.w500,
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 50,
+                      ),
+
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (context) => const Intro2()),);
+
+                        },
+                        child: Container(
+                          alignment: Alignment.center,
+                          height: 50,
+                          width: 320,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            borderRadius: BorderRadius.circular(20)
+                          ),
+                          child: const Text("Next",style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 2,
+                          ),),
+                        ),
+                      ),
+
+                      SizedBox(
+                        height: 50,
+                      ),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CircleAvatar(
+                            radius: 6,
+                            backgroundColor: Colors.orange,
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          CircleAvatar(
+                            radius: 6,
+                            backgroundColor: Colors.grey.shade300,
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          CircleAvatar(
+                            radius: 6,
+                            backgroundColor: Colors.grey.shade300,
+                          )
+                        ],
+                      )
+
+                    ],
+                  ),
+            )));
   }
 }
+
+// ElevatedButton(onPressed: () {
+//
+//
+// Navigator.of(context).push(
+// MaterialPageRoute(builder: (context) => const Intro2()),);
+//
+//
+//
+// }, child: Text('Next',style: TextStyle(
+// fontSize: 25,
+// color: Colors.white
+// ),)),
--- lib/screens/Intro/Views/Screens/intro2.dart
+++ lib/screens/Intro/Views/Screens/intro2.dart
@@ -7,14 +7,107 @@ class Intro2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Intro2'),
-      ),
 
-        body: Center(child: ElevatedButton(onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => const Intro3()));
-        }, child: Text('Next')),)
-    );
+        body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 400,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        image: DecorationImage(image: AssetImage('assets/images/2.png'))),
+                  ),
+
+                  SizedBox(
+                    height: 10,
+                  ),
+
+                  Text(
+                    'Easy To Share',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 2,
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 20,
+                  ),
+
+                  Text(
+                    "            The file App simplifies document management with intuitive tools , secure cloud integration.",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 50,
+                  ),
+
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => const Intro3()));
+
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 50,
+                      width: 320,
+                      decoration: BoxDecoration(
+                          color: Colors.orange,
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                      child: const Text("Next",style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 2,
+                      ),),
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 50,
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        radius: 6,
+                        backgroundColor: Colors.grey.shade300,
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      CircleAvatar(
+                        radius: 6,
+                        backgroundColor: Colors.orange,
+                      ),
+                      SizedBox(
+                        width: 3,
+                      ),
+                      CircleAvatar(
+                        radius: 6,
+                        backgroundColor: Colors.grey.shade300,
+                      )
+                    ],
+                  )
+
+                ],
+              ),
+            )));
+
   }
 }
+
+
