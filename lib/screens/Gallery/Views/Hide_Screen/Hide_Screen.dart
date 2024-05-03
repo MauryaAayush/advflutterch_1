aaeb49157aea9a4144265521801a285121fb1898
Admin <maayush235@gmail.com>
2024-05-03 18:29:00 +0530

    Radhey Radhey

--- lib/screens/Gallery/Views/Hide_Screen/Hide_Screen.dart
+++ lib/screens/Gallery/Views/Hide_Screen/Hide_Screen.dart
@@ -16,7 +16,6 @@ class Hide_Screen extends StatelessWidget {
             style: TextStyle(fontSize: 25),
           ),
         ),
-
         body: Column(
           children: [
             Divider(
@@ -24,6 +23,28 @@ class Hide_Screen extends StatelessWidget {
               thickness: 25,
               color: Colors.grey.shade300,
             ),
+
+            // GridView.builder(gridDelegate: gridDelegate)
+
+            Row(
+              children: [
+                Text('Today',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600))
+              ],
+            ),
+            Row(
+              children: [
+                Container(
+                  height: 90,
+                  width: 85,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                ),
+
+              ],
+            )
           ],
         ),
       ),
